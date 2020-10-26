dc0b7949159e643788a8184a4fca8a53cfe096df
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-26 10:09:03 +0530

    Moved installtion webhook to a different file

--- algobot/__main__.py
+++ algobot/__main__.py
@@ -2,47 +2,38 @@ import asyncio
 import os
 import sys
 import traceback
-from typing import Any
 
 import aiohttp
-from aiohttp import web
 import cachetools
+from aiohttp import web
 from gidgethub import aiohttp as gh_aiohttp
-from gidgethub import routing
-from gidgethub import sansio
-from gidgethub import apps
+from gidgethub import routing, sansio
 
-# Create a Router instance
-# This allows for individual async functions to be written per event type
-# to help keep logic separated and focused instead of having to differentiate
-# between different events manually in user code.
-router = routing.Router()
+from . import check_runs, installations
 
-cache = cachetools.LRUCache(maxsize=500)  # type: cachetools.LRUCache
+router = routing.Router(installations.router, check_runs.router)
+
+lru_cache = cachetools.LRUCache(maxsize=500)  # type: cachetools.LRUCache
+# Timed cache for installation access token (1 hour)
+ttl_cache = cachetools.TTLCache(maxsize=500, ttl=3600)  # type: cachetools.TTLCache
 
 
-async def main(request: web.Request) -> web.Response:
+async def main(request: web.Request):
     try:
         body = await request.read()
         secret = os.environ.get("GITHUB_SECRET")
         event = sansio.Event.from_http(request.headers, body, secret=secret)
-        print("GH delivery ID:", event.delivery_id, file=sys.stderr)
         if event.event == "ping":
             return web.Response(status=200)
-        oauth_token = os.environ.get("GITHUB_OAUTH_TOKEN")
         async with aiohttp.ClientSession() as session:
-            gh = gh_aiohttp.GitHubAPI(
-                session, "TheAlgorithms/Python", oauth_token=oauth_token, cache=cache
-            )
+            gh = gh_aiohttp.GitHubAPI(session, "TheAlgorithms/Python", cache=lru_cache)
             # Give GitHub some time to reach internal consistency.
             await asyncio.sleep(1)
-            await router.dispatch(event, gh)
+            await router.dispatch(event, gh, ttl_cache)
         try:
             print(
                 f"GH requests remaining: {gh.rate_limit.remaining}\n"
                 f"Reset time: {gh.rate_limit.reset_datetime:%b-%d-%Y %H:%M:%S %Z}\n"
-                f"oauth token length: {len(oauth_token)}\n"
-                f"Last 4 digits: {oauth_token[-4:]}\n"
                 f"GH delivery ID: {event.delivery_id}\n"
             )
         except AttributeError:
@@ -51,51 +42,6 @@ async def main(request: web.Request) -> web.Response:
     except Exception:
         traceback.print_exc(file=sys.stderr)
         return web.Response(status=500)
-
-
-@router.register("installation", action="created")
-@router.register("installation_repositories", action="added")
-async def repo_installation_added(
-    event: sansio.Event, gh: gh_aiohttp.GitHubAPI, *args: Any, **kwargs: Any
-):
-    """Give the repository a heads up that the app has been installed.
-
-    :param event: :class:`gidgethub.sansio.Event`,
-                Representation of GitHub's webhook event, access it with event.data
-    :param gh: :class:`gidget.aiohttp.GitHubAPI`, gidgethub GitHub API object
-    """
-    # installation_id = event.data["installation"]["id"]
-    # installation_access_token = await apps.get_installation_access_token(
-    #     gh,
-    #     installation_id=installation_id,
-    #     app_id=os.environ.get("GITHUB_APP_ID"),
-    #     private_key=os.environ.get("GITHUB_PRIVATE_KEY"),
-    # )
-    sender_name = event.data["sender"]["login"]
-    try:
-        repositories = event.data["repositories"]
-    except KeyError:
-        repositories = event.data["repositories_added"]
-
-    for repository in repositories:
-        url = f"/repos/{repository['full_name']}/issues"
-        response = await gh.post(
-            url,
-            data={
-                "title": "Installation successful!",
-                "body": (
-                    f"This is the Algorithms bot at your service! "
-                    f"Thank you for installing me @{sender_name}"
-                ),
-            },
-            # oauth_token=installation_access_token["token"],
-        )
-        issue_url = response["url"]
-        await gh.patch(
-            issue_url,
-            data={"state": "closed"},
-            # oauth_token=installation_access_token["token"],
-        )
 
 
 if __name__ == "__main__":  # pragma: no cover
---
+++ algobot/installations.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+import cachetools
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing, sansio
+
+from . import utils
+
+router = routing.Router()
+
+
+@router.register("installation", action="created")
+@router.register("installation_repositories", action="added")
+async def repo_installation_added(
+    event: sansio.Event,
+    gh: gh_aiohttp.GitHubAPI,
+    cache: cachetools.TTLCache,
+    *args: Any,
+    **kwargs: Any,
+):
+    """Give the repository a heads up that the app has been installed.
+
+    This callback will be triggered by two events:
+    1. When a new installation for this app is added
+    2. When a new repository is added in an existing installation
+    """
+    installation_id = event.data["installation"]["id"]
+    installation_access_token = await utils.get_access_token(gh, installation_id, cache)
+    sender_name = event.data["sender"]["login"]
+    try:
+        repositories = event.data["repositories"]
+    except KeyError:
+        repositories = event.data["repositories_added"]
+
+    for repository in repositories:
+        url = f"/repos/{repository['full_name']}/issues"
+        response = await gh.post(
+            url,
+            data={
+                "title": "Installation successful!",
+                "body": (
+                    f"This is the Algorithms bot at your service! "
+                    f"Thank you for installing me @{sender_name}"
+                ),
+            },
+            oauth_token=installation_access_token,
+        )
+        issue_url = response["url"]
+        await gh.patch(
+            issue_url,
+            data={"state": "closed"},
+            oauth_token=installation_access_token,
+        )
