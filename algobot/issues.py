32e6b414077a60c454d8b7417d6e4255e27ad40a
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-09 11:40:04 +0530

    Add issues event file

---
+++ algobot/issues.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing, sansio
+
+from . import utils
+from .comments import EMPTY_ISSUE_BODY_COMMENT
+
+router = routing.Router()
+
+
+@router.register("issues", action="opened")
+async def close_invalid_issue(
+    event: sansio.Event,
+    gh: gh_aiohttp.GitHubAPI,
+    *args: Any,
+    **kwargs: Any,
+) -> None:
+    """Close an invalid issue.
+
+    An issue is considered invalid if:
+    - It doesn't contain any description.
+    """
+    installation_id = event.data["installation"]["id"]
+    issue = event.data["issue"]
+
+    if not issue["body"]:
+        await utils.close_pr_or_issue(
+            gh,
+            installation_id,
+            comment=EMPTY_ISSUE_BODY_COMMENT.format(user_login=issue["user"]["login"]),
+            pr_or_issue=issue,
+            label="invalid",
+        )
