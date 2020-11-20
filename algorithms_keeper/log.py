e89dcf5e64ad4adafbe14667dfe589742c161c68
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-20 19:18:52 +0530

    Restructured the log functions and global constants

--- algorithms_keeper/log.py
+++ algorithms_keeper/log.py
@@ -8,9 +8,45 @@ from aiohttp.web_response import StreamResponse
 
 CSI = "\033["
 
+MSG_ARGS_FORMAT = {
+    "event": {"color": "green"},
+    "ratelimit": {"color": "yellow", "style": "bold"},
+    "time_remaining": {"color": "yellow", "style": "bold"},
+    "url": {"color": "blue", "style": "underline"},
+    "file": {"color": "yellow"},
+    "request": {"color": "yellow"},
+    "time": {"color": "yellow"},
+    "status": {"color": "", "style": "bold"},  # Determined during runtime.
+}
+
+STATUS_COLOR = {200: "green", 500: "red"}
+
 
 def colorcode(code: int) -> str:
     return CSI + str(code) + "m"
+
+
+def inject_status_color(status: int) -> None:  # pragma: no cover
+    """Inject the color of response status in the color format dictionary."""
+    assert isinstance(status, int), status
+    MSG_ARGS_FORMAT["status"]["color"] = STATUS_COLOR.get(status, "white")
+
+
+def format_args(
+    args: MutableMapping[str, Any], level: str
+) -> Dict[str, str]:  # pragma: no cover
+    """Inject the color and style according to the specification provided in the
+    format dictionary."""
+    # We could directly mutate the `args` dictionary but it is better this way in
+    # the long run.
+    formatted = {}
+    for key, value in args.items():
+        if key in MSG_ARGS_FORMAT:
+            c = MSG_ARGS_FORMAT[key]
+            style = "normal" if "style" not in c else c["style"]
+            value = Color.inject(str(value), c["color"], style, level)
+        formatted[key] = value
+    return formatted
 
 
 class AnsiColor:  # pragma: no cover
@@ -26,6 +62,7 @@ class AnsiColor:  # pragma: no cover
 
     BOLD = colorcode(1)
     DIM = colorcode(2)
+    UNDERLINE = colorcode(4)
     NORMAL = colorcode(22)
     RESET_ALL = colorcode(0)
 
@@ -60,19 +97,11 @@ Color = AnsiColor()
 class CustomAccessLogger(AbstractAccessLogger):  # pragma: no cover
 
     LOG_FORMAT = '%(logger)s "%(request)s" %(status)s %(time)s'
-    COLOR_FORMAT = {
-        "request": {"color": "yellow"},
-        "time": {"color": "yellow"},
-        "status": {"color": "", "style": "bold"},  # Determined during runtime.
-    }
-    STATUS_COLOR = {200: "green", 500: "red"}
 
     def log(self, request: BaseRequest, response: StreamResponse, time: float) -> None:
         """Log the incoming POST request from GitHub."""
         try:
-            self.COLOR_FORMAT["status"]["color"] = self.STATUS_COLOR.get(
-                response.status, "white"
-            )
+            inject_status_color(response.status)
             self.logger.info(
                 self.log_format,
                 {
@@ -92,22 +121,13 @@ class CustomFormatter(logging.Formatter):  # pragma: no cover
     # Time is coming directly from Heroku.
     default_fmt = "[%(levelname)s] %(message)s"
 
-    LOGGING_FORMAT = {
+    LOG_LEVEL_FORMAT = {
         "DEBUG": Color.DEBUG + default_fmt + Color.RESET_ALL,
         "INFO": Color.INFO + default_fmt + Color.RESET_ALL,
         "WARNING": Color.WARNING + default_fmt + Color.RESET_ALL,
         "ERROR": Color.ERROR + default_fmt + Color.RESET_ALL,
         "CRITICAL": Color.CRITICAL + default_fmt + Color.RESET_ALL,
     }
-
-    MSG_ARGS_FORMAT = {
-        "event": {"color": "green"},
-        "ratelimit": {"color": "yellow", "style": "bold"},
-        "time_remaining": {"color": "yellow", "style": "bold"},
-        "url": {"color": "blue"},
-        "file": {"color": "yellow"},
-    }
-    MSG_ARGS_FORMAT.update(CustomAccessLogger.COLOR_FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         """Custom formating for the log message.
@@ -116,11 +136,11 @@ class CustomFormatter(logging.Formatter):  # pragma: no cover
         splits the message according to newline and thus the color format will
         disappear, so add the color format after every newline as well.
         """
-        custom_fmt = self.LOGGING_FORMAT.get(record.levelname, self.default_fmt)
+        custom_fmt = self.LOG_LEVEL_FORMAT.get(record.levelname, self.default_fmt)
         # Inject custom formating to the base class which `self.formatMessage` calls.
         self._style._fmt = custom_fmt
         if record.args and isinstance(record.args, MutableMapping):
-            record.args = self.format_args(record.args, record.levelname)
+            record.args = format_args(record.args, record.levelname)
         msg = record.getMessage()
         if record.exc_info:
             if not record.exc_text:
@@ -137,20 +157,6 @@ class CustomFormatter(logging.Formatter):  # pragma: no cover
             msg += self.formatStack(record.stack_info)
         record.message = msg
         return self.formatMessage(record)
-
-    def format_args(self, args: MutableMapping[str, Any], level: str) -> Dict[str, str]:
-        """Inject the color and style according to the specification provided in the
-        format dictionary."""
-        # We could directly mutate the `args` dictionary but it is better this way in
-        # the long run.
-        formatted = {}
-        for key, value in args.items():
-            if key in self.MSG_ARGS_FORMAT:
-                c = self.MSG_ARGS_FORMAT[key]
-                style = "normal" if "style" not in c else c["style"]
-                value = Color.inject(str(value), c["color"], style, level)
-            formatted[key] = value
-        return formatted
 
 
 handler = logging.StreamHandler()
