3c0b73800e75c3f2de386e627224743b0299e251
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-12-14 15:56:33 +0530

    Add common test framework for testing rules

---
+++ tests/test_rules.py
@@ -0,0 +1,109 @@
+import textwrap
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Type, Union
+
+import pytest
+from fixit import CstLintRule
+from fixit.common.utils import InvalidTestCase, LintRuleCollectionT, ValidTestCase
+from fixit.rule_lint_engine import lint_file
+
+from algorithms_keeper.parser.python_parser import get_rules_from_config
+
+
+@dataclass(frozen=True)
+class GeneratedTestCase:
+    rule: Type[CstLintRule]
+    test_case: Union[ValidTestCase, InvalidTestCase]
+    # This will be used to identify individual tests in the parameterized group.
+    test_case_id: str
+
+
+def _dedent(src: str) -> str:
+    """Remove the leading newline, if present, and all the common leading whitespace
+     from every line in `src`.
+
+    This can be used to make triple-quoted strings line up with the left edge of the
+    display, while still presenting them in the source code in indented form.
+    """
+    if src[0] == "\n":
+        src = src[1:]
+    return textwrap.dedent(src)
+
+
+def _gen_all_test_cases(rules: LintRuleCollectionT) -> List[GeneratedTestCase]:
+    """Generate all the test cases for the provided rules."""
+    all_cases: List[GeneratedTestCase] = []
+    for rule in rules:
+        if not issubclass(rule, CstLintRule):
+            continue
+        for test_type in {"VALID", "INVALID"}:
+            if cases := getattr(rule, test_type, None):
+                for index, test_case in enumerate(cases):
+                    all_cases.append(
+                        GeneratedTestCase(
+                            rule=rule,
+                            test_case=test_case,
+                            test_case_id=f"{test_type}_{index}",
+                        )
+                    )
+    return all_cases
+
+
+@pytest.mark.parametrize(
+    "generated_test",
+    _gen_all_test_cases(get_rules_from_config()),
+    ids=lambda data: f"{data.rule.__name__}_{data.test_case_id}",
+)
+def test_rules(generated_test: GeneratedTestCase) -> None:
+    """Test all the rules with the generated test cases.
+
+    All the test cases comes directly from the `VALID` and `INVALID` attributes for the
+    provided rules. Some of the points to keep in mind:
+
+    - Invalid test case should be written so as to generate only one report.
+    - Attributes should be in all caps: `INVALID` and `VALID`
+    - The code can be written in triple quoted string with indented blocks, they will
+      be removed with the helper function: ``_dedent``
+
+    The logic of the code is the same as that of ``fixit.common.testing`` but this has
+    been converted to using ``pytest`` and removed the fixture feature. This might be
+    added if there's any need for that in the future.
+    """
+    rule = generated_test.rule
+    test_case = generated_test.test_case
+    reports = lint_file(
+        Path(test_case.filename),
+        _dedent(test_case.code).encode("utf-8"),
+        config=test_case.config,
+        rules={rule},
+    )
+
+    if isinstance(test_case, ValidTestCase):
+        assert len(reports) == 0
+    else:
+        assert 0 < len(reports) <= 1
+
+        report = reports[0]  # type: ignore
+
+        if test_case.line is not None:
+            assert test_case.line == report.line
+        if test_case.column is not None:
+            assert test_case.column == report.column
+
+        kind = test_case.kind if test_case.kind is not None else rule.__name__
+        assert kind == report.code
+
+        if test_case.expected_message is not None:
+            assert test_case.expected_message == report.message
+
+        patch = report.patch
+        expected_replacement = test_case.expected_replacement
+
+        if patch is None:
+            assert expected_replacement is None
+        else:
+            assert expected_replacement is not None
+            expected_replacement = _dedent(expected_replacement)
+            patched_code = patch.apply(_dedent(test_case.code))
+            assert patched_code == expected_replacement
