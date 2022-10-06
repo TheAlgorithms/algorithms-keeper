d7684387a482e1f5e60ebe5b4ec1a6c48d34af41
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-10-06 21:05:19 +0530

    chore: remove empty issue check
    As we're using issue forms, they won't be empty ever.

--- algorithms_keeper/event/__init__.py
+++ algorithms_keeper/event/__init__.py
@@ -3,14 +3,12 @@ from gidgethub.routing import Router
 from algorithms_keeper.event.check_run import check_run_router
 from algorithms_keeper.event.commands import commands_router
 from algorithms_keeper.event.installation import installation_router
-from algorithms_keeper.event.issues import issues_router
 from algorithms_keeper.event.pull_request import pull_request_router
 
 main_router: Router = Router(
     check_run_router,
     commands_router,
     installation_router,
-    issues_router,
     pull_request_router,
 )
 
